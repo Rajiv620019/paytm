d584463cb3028de6e74f226a5c047321508bca22
Rajiv S <rajiv620019@gmail.com>
2024-07-03 14:59:21 +0530

    user schema

---
+++ backend/db.js
@@ -0,0 +1,37 @@
+const mongoose = require("mongoose");
+const { string } = require("zod");
+
+const userSchema = new mongoose.Schema({
+  username: {
+    type: string,
+    required: true,
+    unique: true,
+    trim: true,
+    lowercase: true,
+    minLength: 3,
+    maxLength: 30,
+  },
+  password: {
+    type: string,
+    required: true,
+    minLength: 6,
+  },
+  firstName: {
+    type: string,
+    required: true,
+    trim: true,
+    maxLength: 50,
+  },
+  lastName: {
+    type: string,
+    required: true,
+    trim: true,
+    maxLength: 50,
+  },
+});
+
+const User = mongoose.model("User", userSchema);
+
+module.exports = {
+  User,
+};
