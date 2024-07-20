32ff1b19f7f9a144930f5c6296cd62883c1cc253
Rajiv S <rajiv620019@gmail.com>
2024-07-20 22:25:04 +0530

    signin route added

--- backend/routes/user.js
+++ backend/routes/user.js
@@ -13,6 +13,7 @@ const signupBody = zod.object({
   password: zod.string(),
 });
 
+// Signup route
 router.post("/signup", async (req, res) => {
   const { success } = signupBody.safeParse(req.body);
   if (!success) {
@@ -50,6 +51,44 @@ router.post("/signup", async (req, res) => {
   res.json({
     message: "User created successfully",
     token: token,
+  });
+});
+
+const signInBody = zod.object({
+  username: zod.string().email(),
+  password: zod.string(),
+});
+
+// SignIn route
+router.post("/signin", async (req, res) => {
+  const { success } = signInBody.safeParse(req.body);
+
+  if (!success) {
+    return res.status(411).json({
+      message: "Incorrect inputs",
+    });
+  }
+
+  const returnUser = await User.findOne({
+    username: req.body.username,
+    password: req.body.password,
+  });
+
+  if (returnUser) {
+    const token = jwt.sign(
+      {
+        userId: returnUser._id,
+      },
+      JWT_SECRET
+    );
+
+    res.json({
+      token: token,
+    });
+  }
+
+  return res.status(411).json({
+    message: "Error while logging in",
   });
 });
 
