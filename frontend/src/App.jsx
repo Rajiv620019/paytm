758a9fc98579b83808eb6b97668c674efb5ffd5d
Rajiv S <rajiv620019@gmail.com>
2024-07-22 20:28:25 +0530

    Sign & Signup components

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,23 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+
+import SignIn from "./pages/SignIn";
+import Dashboard from "./pages/Dashboard";
+import Send from "./pages/Send";
+import { Signup } from "./pages/Signup";
 
 function App() {
-
   return (
-    <div>
-        Hello world
-    </div>
-  )
+    <>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/signup" element={<Signup />} />
+          <Route path="/signin" element={<SignIn />} />
+          <Route path="/dashboard" element={<Dashboard />} />
+          <Route path="/send" element={<Send />} />
+        </Routes>
+      </BrowserRouter>
+    </>
+  );
 }
 
-export default App
+export default App;
